777ebed4ef313066684e419139ecd0fe4921d3b6
Алексей Попов <a.popov@w1.money>
2022-07-13 11:43:24 +0500

    Добавлены классы работы со стеком

---
+++ Calculator/Stack/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator.Stack
+{
+    public class MyStack<T>
+    {
+        private Node<T> _head;
+        private int _count;
+
+        public bool IsEmpty => _count == 0;
+
+        public int Count => _count;
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = _head;
+
+            _head = node;
+            _count++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+
+            Node<T> temp = _head;
+
+            _head = _head.Next;
+            _count--;
+
+            return temp.Data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+
+            return _head.Data;
+        } 
+    }
+}
